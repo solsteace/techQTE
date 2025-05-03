b5f709e4cd729e406cfc1e8b75fd2d58d903cad4
Allen <valternatean13@gmail.com>
2025-05-04 06:11:53 +0700

    feat: configure CORS
    For now, the only client accepted are localhost-like domains

--- BackendDevelopment/API/index.ts
+++ BackendDevelopment/API/index.ts
@@ -1,5 +1,6 @@
 import { gEnv } from "env.ts";
 import express from "express";
+import cors from "cors";
 import { ErrorHandler } from "src/middlewares/errorHandler.ts";
 import { SimpleLogger } from "src/middlewares/logger.ts";
 import { routerV1 } from "src/routes/index.ts";
@@ -11,6 +12,9 @@ const startTime = Date.now()
 const loggerMiddleware = new SimpleLogger()
 const errorHandler = new ErrorHandler()
 
+APP.use(cors({ // TODO: consider moving the config to env
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"]
+}))
 APP.use(loggerMiddleware.handle.bind(loggerMiddleware))
 
 APP.use(express.urlencoded({extended: false}))
